c475a25b0a41b55e6ab615c96b89916597450b8a
YAMITRAO <work.aky@gmail.com>
2024-08-20 21:40:58 +0530

    Filter routes added

--- routes/admin.js
+++ routes/admin.js
@@ -4,12 +4,12 @@ const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
   res.send(
-    '<form method="POST" action="/product"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add</button></form>'
+    '<form method="POST" action="/admin/product"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add</button></form>'
   );
 });
 router.post("/product", (req, res, next) => {
   console.log(req.body);
-  res.redirect("/");
+  res.redirect("/shop");
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRoute);
-app.use(shopRoute);
+app.use("/admin", adminRoute);
+app.use("/shop", shopRoute);
+
+app.use((req, res, next) => {
+  res.status(404).send("<h2>Page not found</h2>");
+});
 
 app.listen(3000);
