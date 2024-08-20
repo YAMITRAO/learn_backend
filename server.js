93099c261ace31c89b3fe513c25bc0ef2771d432
YAMITRAO <work.aky@gmail.com>
2024-08-20 08:33:20 +0530

    product add and bodyParser is used

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser')
 
-const reListner = (req, res) => {
-    console.log(req.url);
-    res.write('<div>Hello India</div>')
-}
+let app = express();
 
-let server = http.createServer(reListner);
-server.listen(3000);
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use("/add-product", (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="product-name" ><input type="text" name="size"><button type="submit">Add</button></form>')
+})
+
+app.post('/product', (req,res, next)=>{
+    console.log(req.body);
+    // res.send('<div>Product added successfully </div>');
+    res.redirect("/")
+
+})
+
+app.use("/",(req,res, next)=> {
+    console.log("Root based system!...")
+    res.send('<div>Home Page</div>')
+})
+
+app.listen(3000);
