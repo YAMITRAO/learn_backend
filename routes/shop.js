6e3236bdcafacc2aa954fcba2dd921350efac608
YAMITRAO <work.aky@gmail.com>
2024-08-24 21:52:21 +0530

    dynamic add added

--- routes/shop.js
+++ routes/shop.js
@@ -2,12 +2,20 @@ const path = require("path");
 
 const express = require("express");
 
-const rootDir = require("../util/path");
-const adminData = require("../controllers/adminController");
-const shopController = require("../controllers/shopController");
+const shopController = require("../controllers/shop");
 
 const router = express.Router();
 
-router.get("/", shopController.shopController);
+router.get("/", shopController.getIndex);
+
+router.get("/products", shopController.getProducts);
+
+router.get("/products/:productId", shopController.getProduct);
+
+router.get("/cart", shopController.getCart);
+
+router.get("/orders", shopController.getOrders);
+
+router.get("/checkout", shopController.getCheckout);
 
 module.exports = router;
